d6f53da69b129fdc541b09e021fd163676d01d17
eriker75 <eribertmarquez75@gmail.com>
2026-04-07 15:45:23 -0400

    terrior backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { CartModule } from './cart/cart.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { UsersModule } from './users/users.module';
+import { CategoryModule } from './category/category.module';
+import { TagsModule } from './tags/tags.module';
+import { AddressModule } from './address/address.module';
+import { CouponsModule } from './coupons/coupons.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, ProductsModule, NotificationsModule, CartModule, WishlistModule, UsersModule, CategoryModule, TagsModule, AddressModule, CouponsModule],
   controllers: [AppController],
   providers: [AppService],
 })
